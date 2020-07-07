de7960a77e295ab97d89af63543054673db6d145
walid <walid.bouguima@gmail.com>
2020-07-07 18:16:56 +0100

    Add the login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'; 
+import {FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms'; 
+import {ErrorStateMatcher} from '@angular/material/core'; 
+import * as firebase from 'firebase'; 
 
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null , form: FormGroupDirective |NgForm | null): boolean{
+    const isSubmitted = form && form.submitted; 
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted)); 
+
+  }
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup; 
+  nickname = ''; 
+  ref = firebase.database().ref('users/'); 
+  matcher = new MyErrorStateMatcher(); 
 
-  constructor() { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    //initialize the form from the data in the storage
+    if(localStorage.getItem('nickname')){
+      this.router.navigate(['/roomlist']); 
+      ///if there is no data in local storage then set the login form to be populated with the form control values
+    }
+      this.loginForm = this.formBuilder.group({
+        'nickname': [null, Validators.required]
+      }); 
   }
 
+  //A method to submit the login form data to firebase document
+
+  onFormSubmit(form: any){
+    const login = form; 
+    this.ref.orderByChild('nickname').equalTo(login.nickname).once('value', snapshot => {
+      if(snapshot.exists()){
+        localStorage.setItem('nickname', login.nickname);
+      }else{
+        const newUser = firebase.database().ref('users/').push(); 
+        newUser.set(login); 
+        localStorage.setItem('nickname', login.nickname); 
+        this.router.navigate(['/roomlist']);
+      }
+    });
+  }
 }
