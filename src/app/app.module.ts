22de915968b96276adf1fcef9695248a75ba6aa9
walid <walid.bouguima@gmail.com>
2020-07-07 17:38:53 +0100

    Add the required components for the application using cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomlistComponent } from './roomlist/roomlist.component';
+import { AddroomComponent } from './addroom/addroom.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomlistComponent,
+    AddroomComponent,
+    ChatroomComponent
   ],
   imports: [
     BrowserModule,
